282387f7e2ef3df18d95b1351878c9b3fb16f7c7
trombonist76 <mahmut.hg@gmail.com>
2022-11-06 01:01:55 +0300

    [Update] Button new styling props

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -1,23 +1,27 @@
 <script setup>
-  const props = defineProps(["name", "icon", "iconClass", "iconFontSize"])
-  const iconFontSize = {
-    "lg:text-lg": !props.iconFontSize,
-  }
-  const btnClass = [
+  const props = defineProps(["name", "icon", "iconClass", "iconFontSize", "btnGap", "btnPadding"])
+
+  const iconStyle = [
       props.iconFontSize ? props.iconFontSize : "lg:text-lg",
       props.iconClass ? props.iconClass : ""
   ]
+
+  const btnStyle = [
+    props.btnGap ? props.btnGap : "gap-1",
+    props.btnPadding ? props.btnPadding : "py-1 px-4 sm:py-2"
+  ]
+
 </script>
 
 <template>
-  <button class="btn">
-    <span v-if="props.icon" :class="btnClass" class="material-symbols-outlined btn__icon">{{props.icon}}</span>
+  <button class="btn" :class="btnStyle">
+    <span v-if="props.icon" :class="iconStyle" class="material-symbols-outlined btn__icon">{{props.icon}}</span>
     {{props.name}}
   </button>
 </template>
 
 <style lang="scss" scoped>
   .btn{
-    @apply flex items-center rounded-full py-1 gap-1 px-4 sm:py-2;
+    @apply flex items-center rounded-full;
   }
 </style>
