78cec18e64ffcd75922286441c326449d6266de1
trombonist76 <mahmut.hg@gmail.com>
2022-11-11 02:11:08 +0300

    [Update] Renamed CreateTaskModal

---
+++ src/components/Modal/ModalForm.vue
@@ -0,0 +1,28 @@
+<script setup>
+import TaskForm from '@/components/Form/TaskForm.vue'
+import { ref, defineAsyncComponent } from 'vue';
+const props = defineProps(["formTitle", "formComponent"])
+
+
+const FormComponent = defineAsyncComponent(() =>
+  import(`../Form/${props.formComponent}.vue`)
+)
+</script>
+<template>
+    <div class="form">
+      <div class="form__header">
+        <h4>{{props.formTitle}}</h4>
+      </div>
+      <FormComponent/>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+  .new-task{
+    @apply bg-brand m-auto p-8 flex flex-col gap-4 w-[30rem] text-white;
+
+    &__header{
+      @apply flex justify-between items-center;
+    }
+  }
+</style>
