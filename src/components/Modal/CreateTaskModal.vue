ec8cef5c7efe29eae4b723cb3447b330541d4e3b
trombonist76 <mahmut.hg@gmail.com>
2022-11-11 01:29:32 +0300

    [Update] Renamed TaskModal.vue

---
+++ src/components/Modal/CreateTaskModal.vue
@@ -0,0 +1,21 @@
+<script setup>
+import TaskForm from '@/components/Form/TaskForm.vue';
+</script>
+<template>
+    <div class="new-task">
+      <div class="new-task__header">
+        <h4>Add New Task</h4>
+      </div>
+      <TaskForm/>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+  .new-task{
+    @apply bg-brand m-auto p-8 flex flex-col gap-4 w-[30rem] text-white;
+
+    &__header{
+      @apply flex justify-between items-center;
+    }
+  }
+</style>
