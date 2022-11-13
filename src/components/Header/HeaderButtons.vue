f59a8d167a1a7f1fb17a2a95602d41db34b74dbb
trombonist76 <mahmut.hg@gmail.com>
2022-11-14 01:52:21 +0300

    [Update] HeaderDropdown created

--- src/components/Header/HeaderButtons.vue
+++ src/components/Header/HeaderButtons.vue
@@ -2,14 +2,10 @@
 import ButtonComp from "@/components/Button/Button.vue"
 import * as modals from "@/constants";
 import { useMobile } from '@/composables/use-mobile'
-import { useClickOutside } from '@/composables/use-clickOutside'
-import { delay } from "@/utils"
-import { ref } from "vue";
+import HeaderDropdown from "./HeaderDropdown.vue";
 
 const { isMobile } = useMobile()
 const props = defineProps(["openModal", "activeBoardName"])
-const showOptions = ref(false)
-const optionsRef = ref(null)
 
 const openCreateTaskModal = () => {
   props.openModal(modals.CREATE_TASK)
@@ -17,35 +13,23 @@ const openCreateTaskModal = () => {
 
 const openEditBoardModal = () => {
   props.openModal(modals.EDIT_BOARD)
-  toggleOptions()
 }
 
 const openDeleteBoardModal = () => {
   props.openModal(modals.DELETE_BOARD, { name: props.activeBoardName })
-  toggleOptions()
 }
-
-const toggleOptions = async () => {
-  await delay(50)
-  showOptions.value = !showOptions.value
-}
-
-useClickOutside(optionsRef, () => {
-  if(!showOptions.value) return
-  showOptions.value = false
-})
-
 </script>
 
 <template>
   <div class="header__buttons">
     <ButtonComp v-if="isMobile" @click="openCreateTaskModal" class="header__btn" icon="add"></ButtonComp>
     <ButtonComp v-else @click="openCreateTaskModal" class="header__btn" icon="add" name="Add New Task"></ButtonComp>
-    <ButtonComp @click="toggleOptions" icon="more_vert"></ButtonComp>
-    <div v-show="showOptions" ref="optionsRef" class="header__dropdown">
-      <ButtonComp @click="openEditBoardModal" name="Edit Board" class="header__dropdown__btn"></ButtonComp>
-      <ButtonComp @click="openDeleteBoardModal" name="Delete Board" class="header__dropdown__btn header__dropdown__btn--delete"></ButtonComp>
-    </div>
+    <HeaderDropdown 
+      @edit="openEditBoardModal"
+      @delete="openDeleteBoardModal"
+      editName="Edit Board" 
+      deleteName="Delete Board">
+    </HeaderDropdown>
   </div>
 </template>
 
@@ -58,18 +42,6 @@ useClickOutside(optionsRef, () => {
 
   &__btn{
     @apply bg-primary text-xs lg:text-sm
-  }
-
-  &__dropdown{
-    @apply bg-brand-dark border border-border rounded-md p-4 absolute top-16 z-10;
-
-    &__btn{
-      @apply hover:opacity-60 transition-opacity text-secondary-dark;
-
-      &--delete{
-        @apply text-red-400
-      }
-    }
   }
 }
 </style>
---
+++ src/components/Header/HeaderDropdown.vue
@@ -0,0 +1,54 @@
+<script setup>
+import ButtonComp from "@/components/Button/Button.vue"
+import { useClickOutside } from '@/composables/use-clickOutside'
+import { delay } from "@/utils"
+import { ref } from "vue";
+
+const emits = defineEmits("edit", "delete")
+const props = defineProps(["editName", "deleteName"])
+const showOptions = ref(false)
+const optionsRef = ref(null)
+
+const toggleOptions = async () => {
+  await delay(50)
+  showOptions.value = !showOptions.value
+}
+
+useClickOutside(optionsRef, () => {
+  if(!showOptions.value) return
+  showOptions.value = false
+})
+
+const deleteHandler = () => {
+  emits("delete")
+  toggleOptions()
+}
+
+const editHandler = () => {
+  emits("edit")
+  toggleOptions()
+}
+</script>
+
+<template>
+  <ButtonComp icon="more_vert" @click="toggleOptions"></ButtonComp>
+  <div v-show="showOptions" ref="optionsRef" class="dropdown">
+    <ButtonComp @click="editHandler" :name="props.editName" class="dropdown__btn"></ButtonComp>
+    <ButtonComp @click="deleteHandler" :name="props.deleteName" class="dropdown__btn dropdown__btn--delete"></ButtonComp>
+  </div>
+</template>
+
+<style lang="scss" scoped>  
+  .dropdown{
+    @apply bg-brand-dark border border-border rounded-md p-4 absolute top-16 z-10;
+
+    &__btn{
+      @apply hover:opacity-60 transition-opacity text-secondary-dark;
+
+      &--delete{
+        @apply text-red-400
+      }
+    }
+  }
+
+</style>
