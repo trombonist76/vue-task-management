9757787d26b8fa01a1e129b67d082f7518b09f93
trombonist76 <mahmut.hg@gmail.com>
2022-11-14 00:16:58 +0300

    [Update] FormHeader created

---
+++ src/components/Form/FormHeader.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps(["title", "description", "type"])
+const titleClass = computed(() => ({
+  "form__title--delete": props.type === "delete"
+}))
+</script>
+
+<template>
+  <div class="form-header">
+    <h4 :class="titleClass" class="form-title">{{props.title}}</h4>
+  </div>
+  <div v-if="props.description" class="form-description">
+    <p>{{props.description}}</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.form-header{
+  @apply flex justify-between font-bold text-lg items-center;
+}
+
+.form-description{
+  @apply text-sm leading-loose text-secondary-dark;
+}
+
+.form-title{
+  @apply font-bold;
+      
+  &--delete{
+    @apply text-red-400
+  }
+}
+</style>
