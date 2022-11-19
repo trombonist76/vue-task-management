5be4534031cee1677db3280e5f39f42343a2a8a4
trombonist76 <mahmut.hg@gmail.com>
2022-11-20 02:36:08 +0300

    [Update] Change column badge for selected color

--- src/components/Field/Field.vue
+++ src/components/Field/Field.vue
@@ -6,7 +6,7 @@ const props = defineProps(["field"])
 <template>
   <div class="field drop-zone">
     <div class="field__header">
-      <span :class="props.field.color" class="field__circle"></span>
+      <span :style="{ backgroundColor: props.field.color }" class="field__circle"></span>
       {{ props.field.name.toUpperCase() }} ({{ props.field.tasks.length }})
     </div>
     <FieldDraggable :tasks="props.field.tasks"></FieldDraggable>
