d705fefb5176d6539b6db8f65f8cbb514bb54f8e
trombonist76 <mahmut.hg@gmail.com>
2022-11-24 17:41:05 +0300

    [Update] Readme Updated & preview gifs added

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import App from './App.vue'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { plugin, defaultConfig } from '@formkit/vue'
 import './style.css'
 
 const app = createApp(App)
 const pinia = createPinia()
-app.use(plugin, defaultConfig)
 app.use(pinia)
 app.mount('#app')
